33a8b528107cb511341470d6b49c59d87fced242
Vladimir Herasymenko <vol.gerasimenko@gmail.com>
2019-08-24 18:33:40 +0300

    Draw snake

--- js/game-canvas.js
+++ js/game-canvas.js
@@ -1,8 +1,13 @@
 import BaseCanvas from './base-canvas.js'
 
+const copy = obj => JSON.parse(JSON.stringify(obj))
+
 const colors = {
-  border: '#749db3',
-  bg: '#5587a2',
+  square: {
+    fill: '#5286a3',
+    stroke: '#749db3'
+  },
+  bg: '#507f99',
   snake: '#233c4a'
 }
 
@@ -10,11 +15,24 @@ export default class GameCanvas extends BaseCanvas {
   constructor (container) {
     super(container)
 
+    this.snake = {
+      direction: 'right',
+      speedMs: 500,
+      path: [
+        { row: 10, col: 5 },
+        { row: 10, col: 4 },
+        { row: 10, col: 3 }
+      ]
+    }
+
     super.childRenderFunction = this.draw
     this.draw()
+    this.play()
   }
 
   draw () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
     // score and volume on top
     const statsSafeAreaPx = 30
 
@@ -34,7 +52,8 @@ export default class GameCanvas extends BaseCanvas {
     this.ctx.fillStyle = colors.bg
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 
-    // board
+    
+    // draw board grid
     for (let row = 0; row < this.board.rows; row++) {
       for (let col = 0; col < this.board.cols; col++) {
         const square = {
@@ -43,9 +62,57 @@ export default class GameCanvas extends BaseCanvas {
           width: this.squareSizePx,
           height: this.squareSizePx
         }
-        this.ctx.strokeStyle = colors.border
+
+        // square border
+        this.ctx.strokeStyle = colors.square.stroke
         this.ctx.strokeRect(square.x, square.y, square.width, square.height)
+
+        // square bg
+        this.ctx.fillStyle = colors.square.fill
+        this.ctx.fillRect(
+          col * this.squareSizePx + (oddXSpace / 2) + 1,
+          row * this.squareSizePx + statsSafeAreaPx + oddYSpace + 1,
+          this.squareSizePx - 2,
+          this.squareSizePx - 2
+        )
       }
     }
+
+    // draw snake
+    this.ctx.fillStyle = colors.snake
+    this.snake.path.forEach(rect => {
+      this.ctx.fillRect(
+        rect.col * this.squareSizePx + (oddXSpace / 2) + 1,
+        rect.row * this.squareSizePx + statsSafeAreaPx + oddYSpace + 1,
+        this.squareSizePx - 2,
+        this.squareSizePx - 2
+      )
+    })
+
+    requestAnimationFrame(this.draw.bind(this))
+  }
+
+  play () {
+    setInterval(() => {
+      let prevSquare
+      this.snake.path.forEach((square, index) => {
+        if (index === 0) {
+          prevSquare = copy(square)
+
+          switch (this.snake.direction) {
+            case 'up': square.row--; break
+            case 'right': square.col++; break
+            case 'down': square.row++; break
+            case 'left': square.col--; break
+          }
+        } else {
+          const squareBeforeUpdate = copy(this.snake.path[index])
+          this.snake.path[index] = copy(prevSquare)
+          prevSquare = copy(squareBeforeUpdate)
+        }
+      })
+
+      // TODO: add arrow keys controls
+    }, this.snake.speedMs)
   }
 }
